0385882e5089c5cf85ab223ab1c570996437b81b
33357 <43600005+33357@users.noreply.github.com>
2021-07-06 10:56:37 +0800

    0.0.1

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x96f8005E358A2B8e4f7b3d74dF5FE6751887c9ED";
+export declare const INIT_CODE_HASH = "0x2c272808f669dbb141e4759d110f215f9bd446cabd2be19f80565277bfd2139c";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
